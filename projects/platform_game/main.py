92637b60c70518d610a41ff6c187e746e3949c26
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-09-13 18:11:07 -0500

    created the background display
    -added images
    -transformed the scale so the images are responsive to the window
    -displayed the images to the screen

--- projects/platform_game/main.py
+++ projects/platform_game/main.py
@@ -3,9 +3,21 @@ import pygame
 from sys import exit
 from settings import WIDTH, HEIGHT, clock
 
+# Set up
 pygame.init()
-pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Platformer") #temp name, change this
+
+# Background images
+bg_surf1 = pygame.image.load("projects\\platform_game\\graphics\\bg\\sky_solid_color.png").convert_alpha()
+bg_surf1 = pygame.transform.scale(bg_surf1, (WIDTH, HEIGHT))
+bg_surf2 = pygame.image.load("projects\\platform_game\\graphics\\bg\\clouds.png").convert_alpha()
+bg_surf2 = pygame.transform.scale(bg_surf2, (WIDTH, (HEIGHT//3)))
+bg_surf3 = pygame.image.load("projects\\platform_game\\graphics\\bg\\mountain_depth_z_1.png").convert_alpha()
+bg_surf3 = pygame.transform.scale(bg_surf3, (WIDTH, HEIGHT//2))
+bg_surf4 = pygame.image.load("projects\\platform_game\graphics\\bg\\mountain_depth_z_2.png").convert_alpha()
+bg_surf4 = pygame.transform.scale(bg_surf4, (WIDTH, HEIGHT//3))
+
 
 while(1): 
     for event in pygame.event.get(): 
@@ -14,11 +26,15 @@ while(1):
             exit()
 
 
-
-
-
-pygame.display.update()
-clock.tick(60)
+    # Background display
+    screen.blit(bg_surf1, (0,0))
+    screen.blit(bg_surf2, (0,0))
+    screen.blit(bg_surf3, (0,HEIGHT - (HEIGHT//2)))
+    screen.blit(bg_surf4, (0,HEIGHT - (HEIGHT//3)))
+    
+    
+    pygame.display.update()
+    clock.tick(60)
             
 
 
